b2d51d297f36ba325a724e6ee4b7eb2338e9f15d
mhrhpr <130241275+mhrhpr@users.noreply.github.com>
2025-07-31 16:46:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from telegram_handler import run_bot
+from telegram.ext import ApplicationBuilder
+from telegram_handler import register_handlers
 
-if __name__ == "__main__":
-    run_bot()
+async def main():
+    app = ApplicationBuilder().token("8338754838:AAH6WU34i0pqL9sGr-wFOv1sshQSKVyb6Z4").build()
+    register_handlers(app)
+    await app.run_polling()
 
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
