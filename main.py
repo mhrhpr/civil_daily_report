7f053f7c1e3445c71f2175053ab80892a94f1d75
mhrhpr <130241275+mhrhpr@users.noreply.github.com>
2025-07-31 16:53:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from telegram.ext import ApplicationBuilder
-from telegram_handler import register_handlers
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram_handler import start, ask_fields, get_data, ask_another, cancel, ASK_REPORT_TYPE, FILL_DATA, CONFIRM_AGAIN
 
-async def main():
-    app = ApplicationBuilder().token("8338754838:AAH6WU34i0pqL9sGr-wFOv1sshQSKVyb6Z4").build()
-    register_handlers(app)
-    await app.run_polling()
+def run_bot():
+    updater = Updater("8338754838:AAH6WU34i0pqL9sGr-wFOv1sshQSKVyb6Z4", use_context=True)
+    dp = updater.dispatcher
 
-if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            ASK_REPORT_TYPE: [MessageHandler(Filters.text & ~Filters.command, ask_fields)],
+            FILL_DATA: [MessageHandler(Filters.text & ~Filters.command, get_data)],
+            CONFIRM_AGAIN: [MessageHandler(Filters.text & ~Filters.command, ask_another)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dp.add_handler(conv_handler)
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    run_bot()
+
